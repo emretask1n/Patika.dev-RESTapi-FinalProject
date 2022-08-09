6b1dc8a56c14b62bd88927624a49f52a0082bb6d
emretask1n <64936096+emretask1n@users.noreply.github.com>
2022-08-09 03:51:30 +0300

    code edited

--- GivenAmount/router/GivenAmountRouter.go
+++ GivenAmount/router/GivenAmountRouter.go
@@ -5,12 +5,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-//GetGivenAmount shows "GivenAmount" for user
 func GetGivenAmount(c *fiber.Ctx) error {
 	return GivenAmountService.ShowCurrentGivenAmount(c)
 }
 
-//SetGivenAmount changes GivenAmount with a new one
 func SetGivenAmount(c *fiber.Ctx) error {
 	return GivenAmountService.SetNewGivenAmountAndDeleteOldOne(c)
 }
--- Product/router/ProductRouter.go
+++ Product/router/ProductRouter.go
@@ -5,7 +5,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-//GetAllProducts lists all products
 func GetAllProducts(c *fiber.Ctx) error {
 	return ProductService.GetProducts(c)
 }
